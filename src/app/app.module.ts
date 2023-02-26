eef80036b49f46f3a299c5cb7d3c33bb7a3fa7f7
Fabiana1vo <fabianasouzaivo@icloud.com>
2023-02-26 16:11:51 -0300

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    IfRenderComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule,
